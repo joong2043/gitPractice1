cb96a56c733768b08917f6f6e63582c9ca885722
joong2043 <tbvjqlqlqlr@gmail.com>
2022-07-07 15:40:55 +0900

    등록 기능 구현시작

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -14,6 +14,19 @@ public class App {
             String cmd = sc.nextLine().trim();
 
             switch (cmd){
+                case "등록":
+                    System.out.println("명언 : ");
+                    String sentence = sc.nextLine().trim();
+                    System.out.println("작가 : ");
+                    String writer = sc.nextLine().trim();
+                    System.out.println("명언이 등록되었습니다.");
+
+
+                    break;
+
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("------------------");
                 case "종료":
                     break outer;
             }
